a8cf18d51d86ed6578a8ab25892dd7dc879a56b0
soca0_ <150441236+rebio2@users.noreply.github.com>
2025-01-15 13:04:22 +0100

    Update principal.php

--- CRUD-paginacion/app/plantillas/principal.php
+++ CRUD-paginacion/app/plantillas/principal.php
@@ -123,7 +123,7 @@
 
     <!-- Bootstrap JS -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"></script>
-    <script src="web/script.js"></script>
+    <script src="public/script.js"></script>
 </body>
 
 </html>
